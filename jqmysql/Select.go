c5c008d19e02be353f79f9218ee168cc86bd82cd
jinquan <96352189+jinquan0@users.noreply.github.com>
2022-02-25 11:23:26 +0800

    Update Select.go

--- jqmysql/Select.go
+++ jqmysql/Select.go
@@ -5,7 +5,7 @@ import (
     "fmt"
 )
 
-func MySelect(MyDb *sql.DB, sql string, args... interface{}) bool,n_row {
+func MySelect(MyDb *sql.DB, sql string, args... interface{}) bool {
 
     row:=MyDb.QueryRow(sql)
 
@@ -13,14 +13,14 @@ func MySelect(MyDb *sql.DB, sql string, args... interface{}) bool,n_row {
     if err != nil {
         if err == sql.ErrNoRows {
             fmt.Println("MySQL/> There were no rows, but otherwise no error occurred")
-            return false,0   
+            return false
         }else{
             fmt.Println("MySQL/> Query err: %v", err)
-            return false,0
+            return false
         }
     }
 
     ParaLock_my_read_total.Lock(); MyReadTotal++; ParaLock_my_read_total.Unlock()
 
-    return true,1
+    return true
 }
