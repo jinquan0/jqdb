257d3f8b1d250cf1c0212078ecb19895b9493253
jinquan <96352189+jinquan0@users.noreply.github.com>
2022-04-09 20:04:03 +0800

    Create Conn.go

---
+++ jqmssql/Conn.go
@@ -0,0 +1,27 @@
+package jqmssql
+
+import (
+    "database/sql"
+    "fmt"
+    "log"
+    _ "github.com/denisenkom/go-mssqldb"
+)
+
+
+func MssqlConn(server string, port int, user string, pass string) (*sql.DB) {
+    connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d", server, user, pass, port)
+    if *debug {
+        log.Printf(" connString:%s\n", connString)
+    }
+    db, err := sql.Open("mssql", connString)
+    if err != nil {
+        log.Fatal("Open connection failed:", err.Error())
+    }
+    // defer conn.Close()
+    return db
+}
+
+func MssqlDisconn(MsDb *sql.DB){
+    MsDb.Close()
+    log.Println("MSSQL/> database disconnected.")
+}
