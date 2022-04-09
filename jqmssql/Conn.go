85612361e09b05f50455f3a3906688fe95e34f3e
jinquan <96352189+jinquan0@users.noreply.github.com>
2022-04-09 20:09:58 +0800

    Update Conn.go

--- jqmssql/Conn.go
+++ jqmssql/Conn.go
@@ -10,9 +10,7 @@ import (
 
 func MssqlConn(server string, port int, user string, pass string) (*sql.DB) {
     connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d", server, user, pass, port)
-    if *debug {
-        log.Printf(" connString:%s\n", connString)
-    }
+    log.Printf(" connString:%s\n", connString)
     db, err := sql.Open("mssql", connString)
     if err != nil {
         log.Fatal("Open connection failed:", err.Error())
